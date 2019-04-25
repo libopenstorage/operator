07921b4b986bf0715a510277be28d6c0a565561f
Piyush Nimbalkar <piyush@portworx.com>
2019-04-24 18:51:21 -0700

    API group changes for the new sdk

--- pkg/apis/core/register.go
+++
@@ -1,6 +0,0 @@
-package core
-
-const (
-	// GroupName is the group name of the CRD
-	GroupName = "core.libopenstorage.org"
-)
--- pkg/apis/core/v1alpha1/register.go
+++ pkg/apis/core/v1alpha1/register.go
@@ -1,45 +1,23 @@
+// Package v1alpha1 contains API Schema definitions for the core v1alpha1 API group
+// +k8s:deepcopy-gen=package,register
+// +groupName=core.libopenstorage.org
 package v1alpha1
 
 import (
-	"github.com/libopenstorage/operator/pkg/apis/core"
-	sdkK8sutil "github.com/operator-framework/operator-sdk/pkg/util/k8sutil"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"sigs.k8s.io/controller-runtime/pkg/runtime/scheme"
 )
-
-// SchemeGroupVersion is group version used to register these objects
-var SchemeGroupVersion = schema.GroupVersion{Group: core.GroupName, Version: "v1alpha1"}
 
 var (
-	// SchemeBuilder is the scheme builder for the types
-	SchemeBuilder = runtime.NewSchemeBuilder(addKnownTypes)
-	// AddToScheme applies all the stored functions to the scheme
+	// SchemeGroupVersion is group version used to register these objects
+	SchemeGroupVersion = schema.GroupVersion{Group: "core.libopenstorage.org", Version: "v1alpha1"}
+	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
+	SchemeBuilder = &scheme.Builder{GroupVersion: SchemeGroupVersion}
+	// AddToScheme adds all the registered types to the scheme
 	AddToScheme = SchemeBuilder.AddToScheme
 )
-
-// Kind takes an unqualified kind and returns back a Group qualified GroupKind
-func Kind(kind string) schema.GroupKind {
-	return SchemeGroupVersion.WithKind(kind).GroupKind()
-}
 
 // Resource takes an unqualified resource and returns a Group qualified GroupResource
 func Resource(resource string) schema.GroupResource {
 	return SchemeGroupVersion.WithResource(resource).GroupResource()
-}
-
-// Adds the list of known types to Scheme.
-func addKnownTypes(scheme *runtime.Scheme) error {
-	scheme.AddKnownTypes(SchemeGroupVersion,
-		&StorageCluster{},
-		&StorageClusterList{},
-	)
-
-	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
-	return nil
-}
-
-func init() {
-	SchemeBuilder.Register(addKnownTypes)
-	sdkK8sutil.AddToSDKScheme(AddToScheme)
 }
