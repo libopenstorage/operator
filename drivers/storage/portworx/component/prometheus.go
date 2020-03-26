5d0bf593bf7b946e8616b27d7f23178e4964edc4
tusharraut1994 <galaxy.tusharraut@gmail.com>
2020-03-26 23:17:06 +0530

    Added px-backup service monitor selector into Prometheus Instance

--- drivers/storage/portworx/component/prometheus.go
+++ drivers/storage/portworx/component/prometheus.go
@@ -523,7 +523,16 @@ func (c *prometheus) createPrometheusInstance(
 			LogLevel:           "debug",
 			ServiceAccountName: PrometheusServiceAccountName,
 			ServiceMonitorSelector: &metav1.LabelSelector{
-				MatchLabels: serviceMonitorLabels(),
+				MatchExpressions: []metav1.LabelSelectorRequirement{
+					{
+						Key:      "prometheus",
+						Operator: metav1.LabelSelectorOpIn,
+						Values: []string{
+							PxServiceMonitor,
+							PxBackupServiceMonitor,
+						},
+					},
+				},
 			},
 			RuleSelector: &metav1.LabelSelector{
 				MatchLabels: prometheusRuleLabels(),
