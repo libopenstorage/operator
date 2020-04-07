48e443a2ea37fcbb2f03f2c5b38ed1b683e7f896
Thiago Gonzaga <thi_gonzaga@yahoo.com.br>
2020-04-07 15:53:36 -0300

    vendor update
    Signed-off-by: Thiago Gonzaga <thi_gonzaga@yahoo.com.br>

---
+++ vendor/github.com/portworx/sched-ops/k8s/prometheus/servicemonitor.go
@@ -0,0 +1,73 @@
+package prometheus
+
+import (
+	monitoringv1 "github.com/coreos/prometheus-operator/pkg/apis/monitoring/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// ServiceMonitorOps is an interface to perform ServiceMonitor operations
+type ServiceMonitorOps interface {
+	// ListServiceMonitors lists all servicemonitors in a given namespace
+	ListServiceMonitors(namespace string) (*monitoringv1.ServiceMonitorList, error)
+	// GetServiceMonitor gets the service monitor instance that matches the given name
+	GetServiceMonitor(name, namespace string) (*monitoringv1.ServiceMonitor, error)
+	// CreateServiceMonitor creates the given service monitor
+	CreateServiceMonitor(*monitoringv1.ServiceMonitor) (*monitoringv1.ServiceMonitor, error)
+	// UpdateServiceMonitor updates the given service monitor
+	UpdateServiceMonitor(*monitoringv1.ServiceMonitor) (*monitoringv1.ServiceMonitor, error)
+	// DeleteServiceMonitor deletes the given service monitor
+	DeleteServiceMonitor(name, namespace string) error
+}
+
+// ListServiceMonitors lists all servicemonitors in a given namespace
+func (c *Client) ListServiceMonitors(namespace string) (*monitoringv1.ServiceMonitorList, error) {
+	if err := c.initClient(); err != nil {
+		return nil, err
+	}
+
+	return c.prometheus.MonitoringV1().ServiceMonitors(namespace).List(metav1.ListOptions{})
+}
+
+// GetServiceMonitor gets the service monitor instance that matches the given name
+func (c *Client) GetServiceMonitor(name string, namespace string) (*monitoringv1.ServiceMonitor, error) {
+	if err := c.initClient(); err != nil {
+		return nil, err
+	}
+
+	return c.prometheus.MonitoringV1().ServiceMonitors(namespace).Get(name, metav1.GetOptions{})
+}
+
+// CreateServiceMonitor creates the given service monitor
+func (c *Client) CreateServiceMonitor(serviceMonitor *monitoringv1.ServiceMonitor) (*monitoringv1.ServiceMonitor, error) {
+	if err := c.initClient(); err != nil {
+		return nil, err
+	}
+
+	ns := serviceMonitor.Namespace
+	if len(ns) == 0 {
+		ns = corev1.NamespaceDefault
+	}
+
+	return c.prometheus.MonitoringV1().ServiceMonitors(ns).Create(serviceMonitor)
+}
+
+// UpdateServiceMonitor updates the given service monitor
+func (c *Client) UpdateServiceMonitor(serviceMonitor *monitoringv1.ServiceMonitor) (*monitoringv1.ServiceMonitor, error) {
+	if err := c.initClient(); err != nil {
+		return nil, err
+	}
+
+	return c.prometheus.MonitoringV1().ServiceMonitors(serviceMonitor.Namespace).Update(serviceMonitor)
+}
+
+// DeleteServiceMonitor deletes the given service monitor
+func (c *Client) DeleteServiceMonitor(name, namespace string) error {
+	if err := c.initClient(); err != nil {
+		return err
+	}
+
+	return c.prometheus.MonitoringV1().ServiceMonitors(namespace).Delete(name, &metav1.DeleteOptions{
+		PropagationPolicy: &deleteForegroundPolicy,
+	})
+}
