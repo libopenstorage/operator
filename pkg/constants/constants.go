b197f84fb37bdf95a3ea532c7e963fc8000ecfda
Piyush Nimbalkar <pnimbalkar@purestorage.com>
2020-11-04 12:41:12 -0800

    Change the delay before restarting cordoned node pods to 15m
    Signed-off-by: Piyush Nimbalkar <pnimbalkar@purestorage.com>

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -5,5 +5,5 @@ import "time"
 const (
 	// DefaultCordonedRestartDelay duration for which the operator should not try
 	// to restart pods after the node is cordoned
-	DefaultCordonedRestartDelay = 2 * time.Minute
+	DefaultCordonedRestartDelay = 15 * time.Minute
 )
