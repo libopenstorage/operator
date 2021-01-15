d9b6892a4391e2b470066946c6522a3329d2559b
nikolaypopov <nikolay.popov86@gmail.com>
2021-01-14 20:06:06 -0500

    Remove extra unused params
    Signed-off-by: nikolaypopov <nikolay.popov86@gmail.com>

--- test/integration_test/test-deploy.sh
+++ test/integration_test/test-deploy.sh
@@ -1,14 +1,9 @@
 #!/bin/bash -x
 
 test_image_name="openstorage/px-operator-test:latest"
-environment_variables=""
 storage_provisioner="portworx"
 focus_tests=""
-auth_secret_configmap=""
 short_test=false
-cloud_secret=""
-aws_id=""
-aws_key=""
 for i in "$@"
 do
 case $i in
@@ -19,14 +14,8 @@ case $i in
         shift
         ;;
     --storage_provisioner)
-        echo "Flag for clusterdomain test: $2"
+        echo "Storage provisioner to use for test: $2"
         storage_provisioner=$2
-        shift
-        shift
-        ;;
-    --env_vars)
-        echo "Flag for environment variables: $2"
-        environment_variables=$2
         shift
         shift
         ;;
@@ -36,33 +25,9 @@ case $i in
         shift
         shift
         ;;
-    --auth_secret_configmap)
-        echo "K8s secret name to use for test: $2"
-        auth_secret_configmap=$2
-        shift
-        shift
-        ;;
     --short_test)
         echo "Skip tests that are long/not supported: $2"
         short_test=$2
-        shift
-        shift
-        ;;
-    --cloud_secret)
-        echo "Secret name for cloud provider API access: $2"
-        cloud_secret=$2
-        shift
-        shift
-        ;;
-    --aws_id)
-        echo "AWS user id for API access: $2"
-        aws_id=$2
-        shift
-        shift
-        ;;
-    --aws_key)
-        echo "AWS key for API access: $2"
-        aws_key=$2
         shift
         shift
         ;;
@@ -84,30 +49,16 @@ fi
 
 sed -i 's/'SHORT_FLAG'/'"$short_test"'/g' /testspecs/operator-test-pod.yaml
 
-# Configmap with secrets indicates auth-enabled runs are required
-#  * Adding the shared secret to operator-test pod as env variable
-#  * Pass config map to containing px secret operator-test pod so tests can pick up auth-token
-if [ "$auth_secret_configmap" != "" ] ; then
-    sed -i 's/'auth_secret_configmap'/'"$auth_secret_configmap"'/g' /testspecs/operator-test-pod.yaml
-    sed -i 's/'px_shared_secret_key'/'"$AUTH_SHARED_KEY"'/g' /testspecs/operator-test-pod.yaml
-else
-	sed -i 's/'auth_secret_configmap'/'\"\"'/g' /testspecs/operator-test-pod.yaml
-fi
-
 sed -i 's/'storage_provisioner'/'"$storage_provisioner"'/g' /testspecs/operator-test-pod.yaml
 sed -i 's/'username'/'"$SSH_USERNAME"'/g' /testspecs/operator-test-pod.yaml
 sed -i 's/'password'/'"$SSH_PASSWORD"'/g' /testspecs/operator-test-pod.yaml
-sed  -i 's|'openstorage/operator-test:.*'|'"$test_image_name"'|g'  /testspecs/operator-test-pod.yaml
-
-# Add AWS creds to operator-test pod
-sed -i 's/'aws_access_key_id'/'"$aws_id"'/g' /testspecs/operator-test-pod.yaml
-sed -i 's/'aws_secret_access_key'/'"$aws_key"'/g' /testspecs/operator-test-pod.yaml
+sed -i 's|'openstorage/operator-test:.*'|'"$test_image_name"'|g'  /testspecs/operator-test-pod.yaml
 
 kubectl delete -f /testspecs/operator-test-pod.yaml
 kubectl create -f /testspecs/operator-test-pod.yaml
 
 for i in $(seq 1 100) ; do
-    test_status=$(kubectl get pod operator-test -n kube-system -o json | jq ".status.phase" -r)
+    test_status=$(kubectl -n kube-system get pod operator-test -o json | jq ".status.phase" -r)
     if [ "$test_status" == "Running" ] || [ "$test_status" == "Completed" ]; then
         break
     else
@@ -116,19 +67,19 @@ for i in $(seq 1 100) ; do
     fi
 done
 
-kubectl logs operator-test  -n kube-system -f
+kubectl -n kube-system logs -f operator-test
 for i in $(seq 1 100) ; do
-    test_status=$(kubectl get pod operator-test -n kube-system -o json | jq ".status.phase" -r)
+    test_status=$(kubectl -n kube-system get pod operator-test -o json | jq ".status.phase" -r)
     if [ "$test_status" == "Running" ]; then
         echo "Test is still running, status: $test_status"
-        kubectl logs operator-test  -n kube-system -f
+        kubectl -n kube-system logs -f operator-test
     else
         sleep 5
         break
     fi
 done
 
-test_status=$(kubectl get pod operator-test -n kube-system -o json | jq ".status.phase" -r)
+test_status=$(kubectl -n kube-system get pod operator-test -o json | jq ".status.phase" -r)
 if [ "$test_status" == "Succeeded" ]; then
     echo "Tests passed"
     exit 0
