96f62c36bcadcbed8d61777b0cfe128d6dbb48e8
Harsh Desai <harsh@portworx.com>
2021-05-07 16:18:13 -0700

    OPERATOR-339 provide node name dynamically to telemetry container (#304)
    Signed-off-by: Harsh Desai <hadesai@purestorage.com>

--- drivers/storage/portworx/deployment.go
+++ drivers/storage/portworx/deployment.go
@@ -104,6 +104,7 @@ var (
 
 type template struct {
 	cluster         *corev1.StorageCluster
+	nodeName        string
 	isPKS           bool
 	isIKS           bool
 	isOpenshift     bool
@@ -122,12 +123,12 @@ type template struct {
 
 func newTemplate(
 	cluster *corev1.StorageCluster,
-) (*template, error) {
+	nodeName string) (*template, error) {
 	if cluster == nil {
 		return nil, fmt.Errorf("storage cluster cannot be empty")
 	}
 
-	t := &template{cluster: cluster}
+	t := &template{cluster: cluster, nodeName: nodeName}
 
 	var err error
 	var ext string
@@ -220,7 +221,7 @@ func (p *portworx) getNodeByName(nodeName string) (*v1.Node, error) {
 func (p *portworx) GetKVDBPodSpec(
 	cluster *corev1.StorageCluster, nodeName string,
 ) (v1.PodSpec, error) {
-	t, err := newTemplate(cluster)
+	t, err := newTemplate(cluster, nodeName)
 	if err != nil {
 		return v1.PodSpec{}, err
 	}
@@ -258,7 +259,7 @@ func (p *portworx) GetKVDBPodSpec(
 func (p *portworx) GetStoragePodSpec(
 	cluster *corev1.StorageCluster, nodeName string,
 ) (v1.PodSpec, error) {
-	t, err := newTemplate(cluster)
+	t, err := newTemplate(cluster, nodeName)
 	if err != nil {
 		return v1.PodSpec{}, err
 	}
@@ -594,6 +595,12 @@ func (t *template) telemetryContainer() *v1.Container {
 			Privileged: boolPtr(true),
 		},
 		VolumeMounts: t.mountsFromVolInfo(t.getTelemetryVolumeInfoList()),
+	}
+
+	if len(t.nodeName) > 0 {
+		container.Args = []string{
+			fmt.Sprintf("-Dstandalone.controller_sn=%s", t.nodeName),
+		}
 	}
 	return &container
 }
