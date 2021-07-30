42c9b3309cce2f0b873e4df4a248c306d82de166
Grant Griffiths <grant@portworx.com>
2021-07-30 16:43:58 -0700

    [CSI] Remove privileged access from CSI sidecars
    Signed-off-by: Grant Griffiths <grant@portworx.com>

--- drivers/storage/portworx/deployment.go
+++ drivers/storage/portworx/deployment.go
@@ -579,9 +579,6 @@ func (t *template) csiRegistrarContainer() *v1.Container {
 				},
 			},
 		},
-		SecurityContext: &v1.SecurityContext{
-			Privileged: boolPtr(true),
-		},
 		VolumeMounts: []v1.VolumeMount{
 			{
 				Name:      "csi-driver-path",
