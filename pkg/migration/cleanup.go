1a91092c8fe23d7c8c5314ac9e5e1b5daaecaaa2
Piyush Nimbalkar <pnimbalkar@purestorage.com>
2021-12-03 16:03:00 -0800

    OPERATOR-458 Extracting constants for old component names
    Signed-off-by: Piyush Nimbalkar <pnimbalkar@purestorage.com>

--- pkg/migration/cleanup.go
+++ pkg/migration/cleanup.go
@@ -15,6 +15,56 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
+const (
+	pxClusterRoleName           = "node-get-put-list-role"
+	pxClusterRoleBindingName    = "node-role-binding"
+	pxRoleName                  = "px-role"
+	pxRoleBindingName           = "px-role-binding"
+	pxAccountName               = "px-account"
+	pxServiceName               = "portworx-service"
+	pxAPIDaemonSetName          = "portworx-api"
+	pxAPIServiceName            = "portworx-api"
+	storkDeploymentName         = "stork"
+	storkServiceName            = "stork-service"
+	storkConfigName             = "stork-config"
+	storkRoleName               = "stork-role"
+	storkRoleBindingName        = "stork-role-binding"
+	storkAccountName            = "stork-account"
+	schedDeploymentName         = "stork-scheduler"
+	schedRoleName               = "stork-scheduler-role"
+	schedRoleBindingName        = "stork-scheduler-role-binding"
+	schedAccountName            = "stork-scheduler-account"
+	autopilotDeploymentName     = "autopilot"
+	autopilotConfigName         = "autopilot-config"
+	autopilotServiceName        = "autopilot"
+	autopilotRoleName           = "autopilot-role"
+	autopilotRoleBindingName    = "autopilot-role-binding"
+	autopilotAccountName        = "autopilot-account"
+	pvcDeploymentName           = "portworx-pvc-controller"
+	pvcRoleName                 = "portworx-pvc-controller-role"
+	pvcRoleBindingName          = "portworx-pvc-controller-role-binding"
+	pvcAccountName              = "portworx-pvc-controller-account"
+	csiDeploymentName           = "px-csi-ext"
+	csiServiceName              = "px-csi-service"
+	csiRoleName                 = "px-csi-role"
+	csiRoleBindingName          = "px-csi-role-binding"
+	csiAccountName              = "px-csi-account"
+	prometheusOpDeploymentName  = "prometheus-operator"
+	prometheusOpRoleName        = "prometheus-operator"
+	prometheusOpRoleBindingName = "prometheus-operator"
+	prometheusOpAccountName     = "prometheus-operator"
+	prometheusInstanceName      = "prometheus"
+	prometheusServiceName       = "prometheus"
+	prometheusRoleName          = "prometheus"
+	prometheusRoleBindingName   = "prometheus"
+	prometheusAccountName       = "prometheus"
+	serviceMonitorName          = "portworx-prometheus-sm"
+	prometheusRuleName          = "portworx"
+	alertManagerName            = "portworx"
+	alertManagerServiceName     = "alertmanager-portworx"
+	secretsNamespace            = "portworx"
+)
+
 func (h *Handler) deleteComponents(cluster *corev1.StorageCluster) error {
 	if err := h.deleteStorkComponent(cluster); err != nil {
 		return err
@@ -41,171 +91,171 @@ func (h *Handler) deleteComponents(cluster *corev1.StorageCluster) error {
 }
 
 func (h *Handler) deletePortworxRbac(cluster *corev1.StorageCluster) error {
-	if err := h.deleteObject("node-role-binding", "", &rbacv1.ClusterRoleBinding{}, cluster); err != nil {
+	if err := h.deleteObject(pxClusterRoleBindingName, "", &rbacv1.ClusterRoleBinding{}, cluster); err != nil {
 		return err
 	}
-	if err := h.deleteObject("node-get-put-list-role", "", &rbacv1.ClusterRole{}, cluster); err != nil {
+	if err := h.deleteObject(pxClusterRoleName, "", &rbacv1.ClusterRole{}, cluster); err != nil {
 		return err
 	}
-	if err := h.deleteObject("px-role-binding", cluster.Namespace, &rbacv1.RoleBinding{}, cluster); err != nil {
+	if err := h.deleteObject(pxRoleBindingName, cluster.Namespace, &rbacv1.RoleBinding{}, cluster); err != nil {
 		return err
 	}
-	if err := h.deleteObject("px-role", cluster.Namespace, &rbacv1.Role{}, cluster); err != nil {
+	if err := h.deleteObject(pxRoleName, cluster.Namespace, &rbacv1.Role{}, cluster); err != nil {
 		return err
 	}
-	if err := h.deleteObject("px-role-binding", "portworx", &rbacv1.RoleBinding{}, cluster); err != nil {
+	if err := h.deleteObject(pxRoleBindingName, secretsNamespace, &rbacv1.RoleBinding{}, cluster); err != nil {
 		return err
 	}
-	if err := h.deleteObject("px-role", "portworx", &rbacv1.Role{}, cluster); err != nil {
+	if err := h.deleteObject(pxRoleName, secretsNamespace, &rbacv1.Role{}, cluster); err != nil {
 		return err
 	}
-	if err := h.deleteObject("px-account", cluster.Namespace, &v1.ServiceAccount{}, cluster); err != nil {
+	if err := h.deleteObject(pxAccountName, cluster.Namespace, &v1.ServiceAccount{}, cluster); err != nil {
 		return err
 	}
 	return nil
 }
 
 func (h *Handler) deletePortworxAPI(cluster *corev1.StorageCluster) error {
-	if err := h.deleteObject("portworx-api", cluster.Namespace, &appsv1.DaemonSet{}, cluster); err != nil {
+	if err := h.deleteObject(pxAPIDaemonSetName, cluster.Namespace, &appsv1.DaemonSet{}, cluster); err != nil {
 		return err
 	}
-	if err := h.deleteObject("portworx-api", cluster.Namespace, &v1.Service{}, cluster); err != nil {
+	if err := h.deleteObject(pxAPIServiceName, cluster.Namespace, &v1.Service{}, cluster); err != nil {
 		return err
 	}
 	return nil
 }
 
 func (h *Handler) deleteStorkComponent(cluster *corev1.StorageCluster) error {
-	if err := h.deleteObject("stork", cluster.Namespace, &appsv1.Deployment{}, cluster); err != nil {
+	if err := h.deleteObject(storkDeploymentName, cluster.Namespace, &appsv1.Deployment{}, cluster); err != nil {
 		return err
 	}
-	if err := h.deleteObject("stork-service", cluster.Namespace, &v1.Service{}, cluster); err != nil {
+	if err := h.deleteObject(storkServiceName, cluster.Namespace, &v1.Service{}, cluster); err != nil {
 		return err
 	}
-	if err := h.deleteObject("stork-config", cluster.Namespace, &v1.ConfigMap{}, cluster); err != nil {
+	if err := h.deleteObject(storkConfigName, cluster.Namespace, &v1.ConfigMap{}, cluster); err != nil {
 		return err
 	}
-	if err := h.deleteObject("stork-role-binding", "", &rbacv1.ClusterRoleBinding{}, cluster); err != nil {
+	if err := h.deleteObject(storkRoleBindingName, "", &rbacv1.ClusterRoleBinding{}, cluster); err != nil {
 		return err
 	}
-	if err := h.deleteObject("stork-role", "", &rbacv1.ClusterRole{}, cluster); err != nil {
+	if err := h.deleteObject(storkRoleName, "", &rbacv1.ClusterRole{}, cluster); err != nil {
 		return err
 	}
-	if err := h.deleteObject("stork-account", cluster.Namespace, &v1.ServiceAccount{}, cluster); err != nil {
+	if err := h.deleteObject(storkAccountName, cluster.Namespace, &v1.ServiceAccount{}, cluster); err != nil {
 		return err
 	}
-	if err := h.deleteObject("stork-scheduler", cluster.Namespace, &appsv1.Deployment{}, cluster); err != nil {
+	if err := h.deleteObject(schedDeploymentName, cluster.Namespace, &appsv1.Deployment{}, cluster); err != nil {
 		return err
 	}
-	if err := h.deleteObject("stork-scheduler-role-binding", "", &rbacv1.ClusterRoleBinding{}, cluster); err != nil {
+	if err := h.deleteObject(schedRoleBindingName, "", &rbacv1.ClusterRoleBinding{}, cluster); err != nil {
 		return err
 	}
-	if err := h.deleteObject("stork-scheduler-role", "", &rbacv1.ClusterRole{}, cluster); err != nil {
+	if err := h.deleteObject(schedRoleName, "", &rbacv1.ClusterRole{}, cluster); err != nil {
 		return err
 	}
-	if err := h.deleteObject("stork-scheduler-account", cluster.Namespace, &v1.ServiceAccount{}, cluster); err != nil {
+	if err := h.deleteObject(schedAccountName, cluster.Namespace, &v1.ServiceAccount{}, cluster); err != nil {
 		return err
 	}
 	return nil
 }
 
 func (h *Handler) deleteAutopilotComponent(cluster *corev1.StorageCluster) error {
-	if err := h.deleteObject("autopilot", cluster.Namespace, &appsv1.Deployment{}, cluster); err != nil {
+	if err := h.deleteObject(autopilotDeploymentName, cluster.Namespace, &appsv1.Deployment{}, cluster); err != nil {
 		return err
 	}
-	if err := h.deleteObject("autopilot", cluster.Namespace, &v1.Service{}, cluster); err != nil {
+	if err := h.deleteObject(autopilotServiceName, cluster.Namespace, &v1.Service{}, cluster); err != nil {
 		return err
 	}
 	// TODO: The config should be copied to the storage cluster spec before deleting,
 	// so user modifications are persisted
-	if err := h.deleteObject("autopilot-config", cluster.Namespace, &v1.ConfigMap{}, cluster); err != nil {
+	if err := h.deleteObject(autopilotConfigName, cluster.Namespace, &v1.ConfigMap{}, cluster); err != nil {
 		return err
 	}
-	if err := h.deleteObject("autopilot-role-binding", "", &rbacv1.ClusterRoleBinding{}, cluster); err != nil {
+	if err := h.deleteObject(autopilotRoleBindingName, "", &rbacv1.ClusterRoleBinding{}, cluster); err != nil {
 		return err
 	}
-	if err := h.deleteObject("autopilot-role", "", &rbacv1.ClusterRole{}, cluster); err != nil {
+	if err := h.deleteObject(autopilotRoleName, "", &rbacv1.ClusterRole{}, cluster); err != nil {
 		return err
 	}
-	if err := h.deleteObject("autopilot-account", cluster.Namespace, &v1.ServiceAccount{}, cluster); err != nil {
+	if err := h.deleteObject(autopilotAccountName, cluster.Namespace, &v1.ServiceAccount{}, cluster); err != nil {
 		return err
 	}
 	return nil
 }
 
 func (h *Handler) deletePVCControllerComponent(cluster *corev1.StorageCluster) error {
-	if err := h.deleteObject("portworx-pvc-controller", cluster.Namespace, &appsv1.Deployment{}, cluster); err != nil {
+	if err := h.deleteObject(pvcDeploymentName, cluster.Namespace, &appsv1.Deployment{}, cluster); err != nil {
 		return err
 	}
-	if err := h.deleteObject("portworx-pvc-controller-role-binding", "", &rbacv1.ClusterRoleBinding{}, cluster); err != nil {
+	if err := h.deleteObject(pvcRoleBindingName, "", &rbacv1.ClusterRoleBinding{}, cluster); err != nil {
 		return err
 	}
-	if err := h.deleteObject("portworx-pvc-controller-role", "", &rbacv1.ClusterRole{}, cluster); err != nil {
+	if err := h.deleteObject(pvcRoleName, "", &rbacv1.ClusterRole{}, cluster); err != nil {
 		return err
 	}
-	if err := h.deleteObject("portworx-pvc-controller-account", cluster.Namespace, &v1.ServiceAccount{}, cluster); err != nil {
+	if err := h.deleteObject(pvcAccountName, cluster.Namespace, &v1.ServiceAccount{}, cluster); err != nil {
 		return err
 	}
 	return nil
 }
 
 func (h *Handler) deleteCSIComponent(cluster *corev1.StorageCluster) error {
-	if err := h.deleteObject("px-csi-ext", cluster.Namespace, &appsv1.Deployment{}, cluster); err != nil {
+	if err := h.deleteObject(csiDeploymentName, cluster.Namespace, &appsv1.Deployment{}, cluster); err != nil {
 		return err
 	}
-	if err := h.deleteObject("px-csi-service", cluster.Namespace, &v1.Service{}, cluster); err != nil {
+	if err := h.deleteObject(csiServiceName, cluster.Namespace, &v1.Service{}, cluster); err != nil {
 		return err
 	}
-	if err := h.deleteObject("px-csi-role-binding", "", &rbacv1.ClusterRoleBinding{}, cluster); err != nil {
+	if err := h.deleteObject(csiRoleBindingName, "", &rbacv1.ClusterRoleBinding{}, cluster); err != nil {
 		return err
 	}
-	if err := h.deleteObject("px-csi-role", "", &rbacv1.ClusterRole{}, cluster); err != nil {
+	if err := h.deleteObject(csiRoleName, "", &rbacv1.ClusterRole{}, cluster); err != nil {
 		return err
 	}
-	if err := h.deleteObject("px-csi-account", cluster.Namespace, &v1.ServiceAccount{}, cluster); err != nil {
+	if err := h.deleteObject(csiAccountName, cluster.Namespace, &v1.ServiceAccount{}, cluster); err != nil {
 		return err
 	}
 	return nil
 }
 
 func (h *Handler) deleteMonitoringComponent(cluster *corev1.StorageCluster) error {
-	if err := h.deleteObject("portworx-prometheus-sm", cluster.Namespace, &monitoringv1.ServiceMonitor{}, cluster); err != nil {
+	if err := h.deleteObject(serviceMonitorName, cluster.Namespace, &monitoringv1.ServiceMonitor{}, cluster); err != nil {
 		return err
 	}
-	if err := h.deleteObject("portworx", cluster.Namespace, &monitoringv1.PrometheusRule{}, cluster); err != nil {
+	if err := h.deleteObject(prometheusRuleName, cluster.Namespace, &monitoringv1.PrometheusRule{}, cluster); err != nil {
 		return err
 	}
-	if err := h.deleteObject("portworx", cluster.Namespace, &monitoringv1.Alertmanager{}, cluster); err != nil {
+	if err := h.deleteObject(alertManagerName, cluster.Namespace, &monitoringv1.Alertmanager{}, cluster); err != nil {
 		return err
 	}
-	if err := h.deleteObject("alertmanager-portworx", cluster.Namespace, &v1.Service{}, cluster); err != nil {
+	if err := h.deleteObject(alertManagerServiceName, cluster.Namespace, &v1.Service{}, cluster); err != nil {
 		return err
 	}
-	if err := h.deleteObject("prometheus", cluster.Namespace, &monitoringv1.Prometheus{}, cluster); err != nil {
+	if err := h.deleteObject(prometheusInstanceName, cluster.Namespace, &monitoringv1.Prometheus{}, cluster); err != nil {
 		return err
 	}
-	if err := h.deleteObject("prometheus", cluster.Namespace, &v1.Service{}, cluster); err != nil {
+	if err := h.deleteObject(prometheusServiceName, cluster.Namespace, &v1.Service{}, cluster); err != nil {
 		return err
 	}
-	if err := h.deleteObject("prometheus", "", &rbacv1.ClusterRoleBinding{}, cluster); err != nil {
+	if err := h.deleteObject(prometheusRoleBindingName, "", &rbacv1.ClusterRoleBinding{}, cluster); err != nil {
 		return err
 	}
-	if err := h.deleteObject("prometheus", "", &rbacv1.ClusterRole{}, cluster); err != nil {
+	if err := h.deleteObject(prometheusRoleName, "", &rbacv1.ClusterRole{}, cluster); err != nil {
 		return err
 	}
-	if err := h.deleteObject("prometheus", cluster.Namespace, &v1.ServiceAccount{}, cluster); err != nil {
+	if err := h.deleteObject(prometheusAccountName, cluster.Namespace, &v1.ServiceAccount{}, cluster); err != nil {
 		return err
 	}
-	if err := h.deleteObject("prometheus-operator", cluster.Namespace, &appsv1.Deployment{}, cluster); err != nil {
+	if err := h.deleteObject(prometheusOpDeploymentName, cluster.Namespace, &appsv1.Deployment{}, cluster); err != nil {
 		return err
 	}
-	if err := h.deleteObject("prometheus-operator", "", &rbacv1.ClusterRoleBinding{}, cluster); err != nil {
+	if err := h.deleteObject(prometheusOpRoleBindingName, "", &rbacv1.ClusterRoleBinding{}, cluster); err != nil {
 		return err
 	}
-	if err := h.deleteObject("prometheus-operator", "", &rbacv1.ClusterRole{}, cluster); err != nil {
+	if err := h.deleteObject(prometheusOpRoleName, "", &rbacv1.ClusterRole{}, cluster); err != nil {
 		return err
 	}
-	if err := h.deleteObject("prometheus-operator", cluster.Namespace, &v1.ServiceAccount{}, cluster); err != nil {
+	if err := h.deleteObject(prometheusOpAccountName, cluster.Namespace, &v1.ServiceAccount{}, cluster); err != nil {
 		return err
 	}
 	return nil
