bd6413f59991b183f837dc763e10aeaa5011ee69
HaibinMichaelXie <hxie@purestorage.com>
2022-01-10 01:15:09 +0000

    fetch manifest from proxy

--- drivers/storage/portworx/manifest/common.go
+++ drivers/storage/portworx/manifest/common.go
@@ -3,8 +3,14 @@ package manifest
 import (
 	"io/ioutil"
 	"net/http"
+	"net/url"
+	"strings"
 
-	yaml "gopkg.in/yaml.v2"
+	"gopkg.in/yaml.v2"
+)
+
+const (
+	httpPrefix = "http://"
 )
 
 // Methods to override for testing
@@ -12,11 +18,39 @@ var (
 	httpGet = http.Get
 )
 
-func getManifestFromURL(manifestURL string) ([]byte, error) {
-	resp, err := httpGet(manifestURL)
-	if err != nil {
-		return nil, err
+func getManifestFromURL(manifestURL string, proxy string) ([]byte, error) {
+	var resp *http.Response
+	var err error
+	if proxy == "" {
+		resp, err = httpGet(manifestURL)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		// Add the http prefix so we can parse the URL:
+		// Wrong format: url.Parse("127.0.0.1:3213")
+		// Correct format: url.Parse("http://127.0.0.1:3213")
+		if !strings.HasPrefix(strings.ToLower(proxy), httpPrefix) {
+			proxy = httpPrefix + proxy
+		}
+
+		proxyURL, err := url.Parse(proxy)
+		if err != nil {
+			return nil, err
+		}
+
+		client := &http.Client{
+			Transport: &http.Transport{
+				Proxy: http.ProxyURL(proxyURL),
+			},
+		}
+
+		resp, err = client.Get(manifestURL)
+		if err != nil {
+			return nil, err
+		}
 	}
+
 	defer resp.Body.Close()
 	return ioutil.ReadAll(resp.Body)
 }
