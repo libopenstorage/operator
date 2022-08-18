94705141d6f6ba04b0e337fd5f68773c82df6616
HaibinMichaelXie <hxie@purestorage.com>
2022-08-18 00:00:13 +0000

    fix unit test

--- pkg/util/util_test.go
+++ pkg/util/util_test.go
@@ -25,7 +25,7 @@ func TestImageURN(t *testing.T) {
 
 	// TestCase: Empty repo and registry
 	out = getImageURN("", "", "test/image")
-	require.Equal(t, "test/image", out)
+	require.Equal(t, "docker.io/test/image", out)
 
 	// TestCase: Registry without repo but image with repo
 	out = getImageURN("", "registry.io", "test/image")
@@ -117,7 +117,7 @@ func TestImageURNPreserved(t *testing.T) {
 
 	// Ensure original behaviour remains
 	out = getImageURNPreserved("", "", "test/image")
-	require.Equal(t, "test/image", out)
+	require.Equal(t, "docker.io/test/image", out)
 
 	// Ensure original behaviour remains
 	out = getImageURNPreserved("", "registry.io", "test/image")
