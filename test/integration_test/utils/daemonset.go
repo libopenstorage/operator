fabeb6d0ff769227f4717afaca6c94f59094cc19
Jiafeng Liao <jliao@purestorage.com>
2022-09-06 11:26:52 -0700

    [PWX-26833] Update csi-node-driver-registrar image in migration automation

--- test/integration_test/utils/daemonset.go
+++ test/integration_test/utils/daemonset.go
@@ -4,9 +4,9 @@ import (
 	appsv1 "k8s.io/api/apps/v1"
 )
 
-// AddPortworxImageToDaemonSet adds the portworx image from the supplied
+// UpdatePortworxDaemonSetImages adds the portworx image from the supplied
 // version manifest to the given daemonset spec
-func AddPortworxImageToDaemonSet(ds *appsv1.DaemonSet) error {
+func UpdatePortworxDaemonSetImages(ds *appsv1.DaemonSet) error {
 	for i, c := range ds.Spec.Template.Spec.Containers {
 		if c.Name == "portworx" {
 			pxContainer := c.DeepCopy()
@@ -17,7 +17,10 @@ func AddPortworxImageToDaemonSet(ds *appsv1.DaemonSet) error {
 			}
 			pxContainer.Env = env
 			ds.Spec.Template.Spec.Containers[i] = *pxContainer
-			break
+		} else if c.Name == "csi-node-driver-registrar" {
+			container := c.DeepCopy()
+			container.Image = PxSpecImages["csiNodeDriverRegistrar"]
+			ds.Spec.Template.Spec.Containers[i] = *container
 		}
 	}
 	return nil
