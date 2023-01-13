b3e56da8abdb16a6b6dceb05f1c3ae1dbd7a2997
Shivanjan Chakravorty <schakravorty@purestorage.com>
2023-01-13 04:40:18 -0700

    PWX-28101 read custom kubelet path from env
    Signed-off-by: Shivanjan Chakravorty <schakravorty@purestorage.com>

--- drivers/storage/portworx/util/util.go
+++ drivers/storage/portworx/util/util.go
@@ -66,6 +66,8 @@ const (
 	EssentialsUserIDKey = "px-essen-user-id"
 	// EssentialsOSBEndpointKey is the secret key for Essentials OSB endpoint
 	EssentialsOSBEndpointKey = "px-osb-endpoint"
+	// EnvKeyKubeletDir env var to set custom kubelet directory
+	EnvKeyKubeletDir = "KUBELET_DIR"
 
 	// AnnotationIsPKS annotation indicating whether it is a PKS cluster
 	AnnotationIsPKS = pxAnnotationPrefix + "/is-pks"
@@ -407,6 +409,13 @@ func KubeletPath(cluster *corev1.StorageCluster) string {
 	if IsPKS(cluster) {
 		return "/var/vcap/data/kubelet"
 	}
+
+	for _, env := range cluster.Spec.Env {
+		if env.Name == EnvKeyKubeletDir && len(env.Value) > 0 {
+			return env.Value
+		}
+	}
+
 	return "/var/lib/kubelet"
 }
 
