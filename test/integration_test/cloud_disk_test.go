17c8c80c1f80ead685d05a091c362cb353c07942
Dev Kant Chouhan <dchouhan@dev-dchouhan.dev.purestorage.com>
2023-02-05 23:22:13 -0700

    WIP

--- test/integration_test/cloud_disk_test.go
+++ test/integration_test/cloud_disk_test.go
@@ -11,6 +11,10 @@ import (
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/require"
 
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/libopenstorage/cloudops"
+	awsops "github.com/libopenstorage/cloudops/aws"
+	gceops "github.com/libopenstorage/cloudops/gce"
 	corev1 "github.com/libopenstorage/operator/pkg/apis/core/v1"
 	"github.com/libopenstorage/operator/pkg/util/test"
 	"github.com/libopenstorage/operator/test/integration_test/types"
@@ -18,8 +22,11 @@ import (
 )
 
 var (
-	disksEncAzure = []string{"type=Premium_LRS,size=100,diskEncryptionSetID=invalid"}
-	disksEncGce   = []string{"type=pd-standard,size=150,kms=invalid"}
+	disksEncAzure  = []string{"type=Premium_LRS,size=100,diskEncryptionSetID=invalid"}
+	disksEncGce    = []string{"type=pd-standard,size=150,kms=invalid"}
+	disksTagsAzure = []string{"type=Premium_LRS,size=65,tags=key:val;"}
+	disksTagsEks   = []string{"type=gp3,size=55,tags=key:val;"}
+	disksTagsGce   = []string{""}
 )
 
 var TestCloudDiskEncryptionCases = []types.TestCase{
@@ -136,6 +143,101 @@ func EncryptedDiskInstallPass(tc *types.TestCase) func(*testing.T) {
 		// Wipe PX and validate
 		logrus.Infof("Uninstall StorageCluster %s", cluster.Name)
 		ci_utils.UninstallAndValidateStorageCluster(cluster, t)
+	}
+}
+
+var TestDiskTaggingCases = []types.TestCase{
+	{
+		TestName:        "SimpleTags",
+		TestrailCaseIDs: []string{},
+		TestSpec: func(t *testing.T) interface{} {
+			cluster := &corev1.StorageCluster{}
+			cluster.Name = "px-cluster-simpletags"
+			err := ci_utils.ConstructStorageCluster(cluster, ci_utils.PxSpecGenURL, ci_utils.PxSpecImages)
+			require.NoError(t, err)
+			if ci_utils.IsAks {
+				cluster.Spec.CloudStorage = &corev1.CloudStorageSpec{
+					CloudStorageCommon: corev1.CloudStorageCommon{
+						DeviceSpecs: &disksTagsAzure,
+					},
+				}
+			}
+			if ci_utils.IsGke {
+				cluster.Spec.CloudStorage = &corev1.CloudStorageSpec{
+					CloudStorageCommon: corev1.CloudStorageCommon{
+						DeviceSpecs: &disksTagsGce,
+					},
+				}
+			}
+
+			if ci_utils.IsEks {
+				cluster.Spec.CloudStorage = &corev1.CloudStorageSpec{
+					CloudStorageCommon: corev1.CloudStorageCommon{
+						DeviceSpecs: &disksTagsEks,
+					},
+				}
+			}
+
+			return cluster
+		},
+		TestFunc: CheckTags,
+		ShouldSkip: func(tc *types.TestCase) bool {
+			return !ci_utils.IsEks
+		},
+	},
+}
+
+func CheckTags(tc *types.TestCase) func(*testing.T) {
+	return func(t *testing.T) {
+		testSpec := tc.TestSpec(t)
+		cluster, ok := testSpec.(*corev1.StorageCluster)
+		require.True(t, ok)
+
+		logrus.Infof("Deploy and validate StorageCluster %s ", cluster.Name)
+		cluster = ci_utils.DeployAndValidateStorageCluster(cluster, ci_utils.PxSpecImages, t)
+
+		logrus.Infof("Checking tags on disk")
+		labelClusterUID := "labelClusterUID"
+
+		if ci_utils.IsEks {
+			awsClient, err := awsops.NewClient()
+			require.NoError(t, err)
+			result, err := awsClient.Enumerate(nil, map[string]string{labelClusterUID: string(cluster.UID)}, "")
+			require.NoError(t, err)
+			volumes := result[cloudops.SetIdentifierNone]
+			require.Greater(t, len(volumes), 0, "No volume found")
+			if len(volumes) > 0 {
+				logrus.Infof("Found %v volumes", len(volumes))
+				for _, vol := range volumes {
+					ec2Vol := vol.(*ec2.Volume)
+					tags, err := awsClient.Tags(*ec2Vol.VolumeId)
+					require.NoError(t, err)
+					if tags["pxtype"] == "data" {
+						require.Equal(t, tags["key"], "val", "Volume Tag did not have the correct value")	
+					}
+				}
+			}
+		}
+
+		if ci_utils.IsGke {
+			gkeClient, err := gceops.NewClient()
+			require.NoError(t, err)
+			result, err := gkeClient.Enumerate(nil, map[string]string{labelClusterUID: string(cluster.UID)}, "")
+			require.NoError(t, err)
+			volumes := result[cloudops.SetIdentifierNone]
+			require.Greater(t, len(volumes), 0, "No volume found")
+			if len(volumes) > 0 {
+				logrus.Infof("Found %v volumes", len(volumes))
+				for _, vol := range volumes {
+					ec2Vol := vol.(*g.Volume)
+					tags, err := awsClient.Tags(*ec2Vol.VolumeId)
+					require.NoError(t, err)
+					if tags["pxtype"] == "data" {
+						require.Equal(t, tags["key"], "val", "Volume Tag did not have the correct value")	
+					}
+				}
+			}
+		}
 	}
 }
 
