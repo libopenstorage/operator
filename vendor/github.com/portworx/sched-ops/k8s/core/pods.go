28c519e75a1704aa1b70ae6779167e5f6bc5e44e
Zoran Rajic <zox@portworx.com>
2023-03-07 12:08:38 -0800

    Vendoring in latest sched-ops

--- vendor/github.com/portworx/sched-ops/k8s/core/pods.go
+++ vendor/github.com/portworx/sched-ops/k8s/core/pods.go
@@ -85,11 +85,11 @@ type RunCommandInPodExRequest struct {
 	Command       []string
 	PODName       string
 	ContainerName string
-	Namespace string
-	UseTTY bool
-	Stdin  io.Reader
-	Stdout io.Writer
-	Stderr io.Writer
+	Namespace     string
+	UseTTY        bool
+	Stdin         io.Reader
+	Stdout        io.Writer
+	Stderr        io.Writer
 }
 
 // CreatePod creates the given pod.
@@ -232,8 +232,16 @@ func (c *Client) getPodsUsingPVCWithListOptions(pvcName, pvcNamespace string, op
 	for _, p := range pods.Items {
 		for _, v := range p.Spec.Volumes {
 			if v.PersistentVolumeClaim != nil && v.PersistentVolumeClaim.ClaimName == pvcName {
-				retList = append(retList, p)
-				break
+				// Along PVC present in the volume list, we also checking whether any of the container in the
+				// pod is really using it by mount them.
+				for _, container := range p.Spec.Containers {
+					for _, mount := range container.VolumeMounts {
+						if mount.Name == v.Name {
+							retList = append(retList, p)
+							break
+						}
+					}
+				}
 			}
 		}
 	}
