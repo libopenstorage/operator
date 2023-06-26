775358569d12f3b473535bb7c801eafdbc7f6754
Harsh Desai <hadesai@purestorage.com>
2023-06-26 10:00:03 -0700

    Pwx 31931 preflight on fresh install only (#1084) (#1091)
    * PWX-31931: Only execute pre-flight on fresh PX install.
    
    
    
    * PWX-31931: Ensure Annotation map exists.
    
    
    
    * PWX-31931: If skipping pre-flight only set the annotation to false if its not set or empty.
    
    
    
    * PWX-31931: Use Piyush review code.
    
    
    
    ---------
    
    Signed-off-by: Jose Rivera <jose@portworx.com>
    Co-authored-by: Jose Rivera (Hose) <jose@portworx.com>
    Co-authored-by: Piyush Nimbalkar <pnimbalkar@purestorage.com>

--- pkg/controller/storagecluster/storagecluster.go
+++ pkg/controller/storagecluster/storagecluster.go
@@ -631,13 +631,20 @@ func (c *Controller) syncStorageCluster(
 	}
 
 	pxVer30, _ := version.NewVersion("3.0")
-	if pxutil.GetPortworxVersion(cluster).GreaterThanOrEqual(pxVer30) { // Preflight should only run on PX version 3.0.0 and above
+	if pxutil.IsFreshInstall(cluster) && pxutil.GetPortworxVersion(cluster).GreaterThanOrEqual(pxVer30) { // Preflight should only run on PX version 3.0.0 and above
 		// If preflight failed, or previous check failed, reconcile would stop here until issues got resolved
 		if err := c.runPreflightCheck(cluster); err != nil {
 			if updateErr := c.updateStorageClusterState(cluster, corev1.ClusterStateDegraded); updateErr != nil {
 				logrus.Errorf("failed to update StorageCluster status. %v", updateErr)
 			}
 			return fmt.Errorf("preflight check failed for StorageCluster %v/%v: %v", cluster.Namespace, cluster.Name, err)
+		}
+	} else {
+		if _, exists := cluster.Annotations[pxutil.AnnotationPreflightCheck]; !exists {
+			if cluster.Annotations == nil {
+				cluster.Annotations = make(map[string]string)
+			}
+			cluster.Annotations[pxutil.AnnotationPreflightCheck] = "false"
 		}
 	}
 
