9e5795c73ce55a3da2c615c4d032efb2142fe25b
Jose Rivera (Hose) <jose@portworx.com>
2023-06-30 10:43:13 -0700

    PWX-32051: move default ccm port 9029 and create ccm cm before PX start (#1105) (#1109)
    * PWX-32051: If PX version is 3.0 or greater use 9029 for CCM uploader port.
    
    
    
    * PWX-32051: Create configmap for px-telemetry-phonehome before PX oci-mon pod launches. So it can be mounted in oci-mon container for reference.
    
    
    
    * PWX-32051: Only create/update the uploader cm in one place and preserve the 'restart' flag and pass it thrugh when reconciling uploader cm.  Fix UTs
    
    
    
    ---------
    
    Signed-off-by: Jose Rivera <jose@portworx.com>

--- drivers/storage/portworx/deployment.go
+++ drivers/storage/portworx/deployment.go
@@ -1193,6 +1193,7 @@ func (t *template) getVolumeMounts() []v1.VolumeMount {
 		t.getIKSVolumeInfoList,
 		t.getPKSVolumeInfoList,
 		t.getBottleRocketVolumeInfoList,
+		t.getTelemetryPhoneHomeVolumeInfoList,
 	}
 	for _, fn := range extensions {
 		volumeInfoList = append(volumeInfoList, fn()...)
@@ -1253,6 +1254,7 @@ func (t *template) getVolumes() []v1.Volume {
 		t.getIKSVolumeInfoList,
 		t.getPKSVolumeInfoList,
 		t.getBottleRocketVolumeInfoList,
+		t.getTelemetryPhoneHomeVolumeInfoList,
 	}
 	for _, fn := range extensions {
 		volumeInfoList = append(volumeInfoList, fn()...)
@@ -1368,6 +1370,29 @@ func (t *template) getCSIVolumeInfoList() []volumeInfo {
 		},
 	)
 	return volumeInfoList
+}
+
+func (t *template) getTelemetryPhoneHomeVolumeInfoList() []volumeInfo {
+	if pxutil.IsTelemetryEnabled(t.cluster.Spec) {
+		return []volumeInfo{
+			{
+				name:      "ccm-phonehome-config",
+				mountPath: "/etc/ccm",
+				configMapType: &v1.ConfigMapVolumeSource{
+					LocalObjectReference: v1.LocalObjectReference{
+						Name: component.ConfigMapNameTelemetryPhonehome,
+					},
+					Items: []v1.KeyToPath{
+						{
+							Key:  component.TelemetryPropertiesFilename,
+							Path: component.TelemetryPropertiesFilename,
+						},
+					},
+				},
+			},
+		}
+	}
+	return []volumeInfo{}
 }
 
 func (t *template) getTelemetryVolumeInfoList() []volumeInfo {
