adea83326d533e29d9dd981f683459166bbdee51
Zoran Rajic <zox@portworx.com>
2023-07-16 16:01:16 -0700

    PWX-27672: Replacing privileged:true switch for Portworx PODs
    * Portworx PODs (oci-monitors) no longer using `privileged:true` - but using 5 capabilities instead
    * adding new `STC.security.privileged = true` switch, to roll back to the original `privileged:true` security-setting
    * we're also disabling all Bidirectional-mounts, and converting them to regular-mounts  (warning will indicate to change STC...privileged=true to enable back)
    
    Signed-off-by: Zoran Rajic <zrajic@purestorage.com>

--- pkg/constants/metadata.go
+++ pkg/constants/metadata.go
@@ -37,6 +37,8 @@ const (
 	// the custom registry, there is a list of hardcoded common registries, however the list
 	// may not be complete, users can use this annotation to add more.
 	AnnotationCommonImageRegistries = OperatorPrefix + "/common-image-registries"
+	// AnnotationAppArmorPrefix controls which AppArmor profile will be used per container.
+	AnnotationAppArmorPrefix = "container.apparmor.security.beta.kubernetes.io/"
 )
 
 const (
