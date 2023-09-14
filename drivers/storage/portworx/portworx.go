e1e960272d145a2ebebff2d98db9f0dc1bbefe67
Jose Rivera (Hose) <jose@portworx.com>
2023-09-14 15:50:03 -0700

    PWX-33374:Set Security context for OCP before pre-flight is launched. (#1264)
    * PWX-33374:Set Security context for OCP before pre-flight is launched.
    
    Signed-off-by: Jose Rivera <jose@portworx.com>
    
    * PWX-33374: Remove the run-once attempt cond.  This is did not work accross node-wipes and also is not necessary.    Handle critical error from security component reconcile.
    
    Signed-off-by: Jose Rivera <jose@portworx.com>
    
    * PWX-33374: Change function name to represent what the function does.
    
    Signed-off-by: Jose Rivera <jose@portworx.com>
    
    ---------
    
    Signed-off-by: Jose Rivera <jose@portworx.com>

--- drivers/storage/portworx/portworx.go
+++ drivers/storage/portworx/portworx.go
@@ -91,6 +91,22 @@ func (p *portworx) Init(
 	return nil
 }
 
+func createSecurityContextForValidate(recorder record.EventRecorder, cluster *corev1.StorageCluster) error {
+	if secComp, exists := component.Get(component.SCCComponentName); exists {
+		if secComp.IsEnabled(cluster) {
+			err := secComp.Reconcile(cluster)
+			if ce, ok := err.(*component.Error); ok &&
+				ce.Code() == component.ErrCritical {
+				return fmt.Errorf("pre-flight: %s component setup failed: %v", secComp.Name(), err)
+			} else if err != nil {
+				msg := fmt.Sprintf("pre-flight: failed to setup %s. %v", secComp.Name(), err)
+				k8sutil.WarningEvent(recorder, cluster, util.FailedComponentReason, msg)
+			}
+		}
+	}
+	return nil
+}
+
 func (p *portworx) Validate(cluster *corev1.StorageCluster) error {
 
 	condition := &corev1.ClusterCondition{
@@ -131,6 +147,12 @@ func (p *portworx) Validate(cluster *corev1.StorageCluster) error {
 		logrus.Infof(condition.Message)
 		deletePreflight()
 		return nil
+	}
+
+	if err := createSecurityContextForValidate(p.recorder, cluster); err != nil {
+		setClusterCondition(corev1.ClusterConditionStatusFailed, err.Error())
+		deletePreflight()
+		return err
 	}
 
 	// Start the pre-flight container. The pre-flight checks at this time are specific to enabling DMthin
