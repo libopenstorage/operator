edd2bc06691e9983762747bf7d503675c93ab57b
Zoran Rajic <zrajic@purestorage.com>
2024-03-06 20:52:50 -0800

    fixing imports

--- drivers/storage/portworx/portworx_test.go
+++ drivers/storage/portworx/portworx_test.go
@@ -25,6 +25,7 @@ import (
 	operatorops "github.com/portworx/sched-ops/k8s/operator"
 	monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
 	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	appsv1 "k8s.io/api/apps/v1"
 	v1 "k8s.io/api/core/v1"
