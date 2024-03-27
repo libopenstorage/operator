1d359da5a20f88a460886903b982ac132b87fe1c
svijaykumar-px <svijaykumar@purestorage.com>
2024-03-27 07:00:34 +0000

    adding k8snode name label to px pods

--- pkg/constants/metadata.go
+++ pkg/constants/metadata.go
@@ -52,6 +52,9 @@ const (
 	OperatorLabelManagedByKey = OperatorPrefix + "/managed-by"
 	// OperatorLabelManagedByValue indicates that the object is managed by portworx.
 	OperatorLabelManagedByValue = "portworx"
+	// K8sNodeRunningPxPod hold the value of the kubernetes node on whcih the portworx pod is running
+    // Used for creating a Node PDB
+    K8sNodeRunningPxPod = "k8s-node-running-px-pod"
 )
 
 const (
--- pkg/controller/storagecluster/storagecluster.go
+++ pkg/controller/storagecluster/storagecluster.go
@@ -1393,6 +1393,7 @@ func (c *Controller) CreatePodTemplate(
 	podSpec.NodeName = node.Name
 	labels := c.StorageClusterSelectorLabels(cluster)
 	labels[constants.OperatorLabelManagedByKey] = constants.OperatorLabelManagedByValue
+	labels[constants.K8sNodeRunningPxPod] = node.Name
 	newTemplate := v1.PodTemplateSpec{
 		ObjectMeta: metav1.ObjectMeta{
 			Namespace:   cluster.Namespace,
