7232e5c43f5b6a30311da94224f60077725316fa
Swarupa Vijaykumar <123443458+svijaykumar-px@users.noreply.github.com>
2024-04-02 11:35:55 +0530

    PWX-36170: Adding k8s node name as label to all px pods (#1503)
    * adding k8snode name label to px pods
    
    * correcting go fmt error
    
    * making requested changes

--- pkg/controller/storagecluster/storagecluster.go
+++ pkg/controller/storagecluster/storagecluster.go
@@ -1441,6 +1441,7 @@ func (c *Controller) CreatePodTemplate(
 	podSpec.NodeName = node.Name
 	labels := c.StorageClusterSelectorLabels(cluster)
 	labels[constants.OperatorLabelManagedByKey] = constants.OperatorLabelManagedByValue
+	labels[constants.OperatorLabelNodeNameKey] = node.Name
 	newTemplate := v1.PodTemplateSpec{
 		ObjectMeta: metav1.ObjectMeta{
 			Namespace:   cluster.Namespace,
