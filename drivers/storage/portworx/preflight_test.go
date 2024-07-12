064584d92d8d96788e43da92e9bba90c505142f8
shsun_pure <shsun@purestorage.com>
2024-07-12 17:49:49 +0000

    fix metadataDeviceSpec without facd pod
    Signed-off-by: shsun_pure <shsun@purestorage.com>

--- drivers/storage/portworx/preflight_test.go
+++ drivers/storage/portworx/preflight_test.go
@@ -13,6 +13,7 @@ import (
 func TestDmthinFacdDefCmeta(t *testing.T) {
 	fakeRecorder := record.NewFakeRecorder(10)
 
+	// with pod name
 	u := &preFlightPortworx{
 		cluster: &corev1.StorageCluster{
 			Spec: corev1.StorageClusterSpec{
@@ -47,4 +48,40 @@ func TestDmthinFacdDefCmeta(t *testing.T) {
 	err := u.ProcessPreFlightResults(fakeRecorder, storageNodes)
 	require.Nil(t, err)
 	require.Equal(t, fmt.Sprintf("%s,%s", DefCmetaFACD, "pod=testpod"), *u.cluster.Spec.CloudStorage.SystemMdDeviceSpec)
+
+	// without pod name
+	u = &preFlightPortworx{
+		cluster: &corev1.StorageCluster{
+			Spec: corev1.StorageClusterSpec{
+				CommonConfig: corev1.CommonConfig{
+					Env: []v1.EnvVar{
+						{
+							Name:  "PURE_FLASHARRAY_SAN_TYPE",
+							Value: "PURE_FLASHARRAY_SAN_TYPE",
+						},
+					},
+				},
+				CloudStorage: &corev1.CloudStorageSpec{
+					CloudStorageCommon: corev1.CloudStorageCommon{
+						DeviceSpecs: &[]string{"size=49"},
+					},
+				},
+			},
+		},
+		hardFail: true,
+	}
+	storageNodes = []*corev1.StorageNode{
+		{
+			Status: corev1.NodeStatus{
+				Checks: []corev1.CheckResult{
+					{
+						Type: "status",
+					},
+				},
+			},
+		},
+	}
+	err = u.ProcessPreFlightResults(fakeRecorder, storageNodes)
+	require.Nil(t, err)
+	require.Equal(t, DefCmetaFACD, *u.cluster.Spec.CloudStorage.SystemMdDeviceSpec)
 }
