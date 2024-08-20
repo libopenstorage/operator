74c2ee733d6c423567e3d4fc50658f85f59975f3
Max Morton <max.morton@purestorage.com>
2024-08-20 00:06:30 +0000

    fix TestTelemetryCCMGoRestartPhonehome

--- drivers/storage/portworx/components_test.go
+++ drivers/storage/portworx/components_test.go
@@ -17420,7 +17420,8 @@ func TestTelemetryCCMGoRestartPhonehome(t *testing.T) {
 			StartPort: &startPort,
 		},
 		Status: corev1.StorageClusterStatus{
-			ClusterUID: "test-clusteruid",
+			ClusterUID:  "test-clusteruid",
+			ClusterName: "test-clustername",
 		},
 	}
 
